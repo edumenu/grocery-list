9111921b352be800d393772b64753ea6aa11d7ff
Edem Dumenu <ehdemdume@gmail.com>
2020-07-03 11:31:16 -0400

    Dahsboard UI
    Completed the dashboard UI

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/images/groceryImage.png';
 
 function Header() {
@@ -6,29 +6,31 @@ function Header() {
 
     return (
         <nav className="flex items-center justify-between flex-wrap p-6">
-          <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <img className="mx-auto my-auto w-12" src={logo} alt="logo" />
-            {/* <a href="/" className="font-bold text-white text-xl tracking-tight hover:text-yellow-400">Grocery</a> */}
-          </div>
-          <div className="block lg:hidden md:hidden">
-            <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
-              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-            </button>
-          </div>
-          <div className={"w-full block lg:w-auto md:w-auto sm:auto "}>
-            <div className="text-lg lg:flex-grow md:flex-grow sm:flew-grow">
-              <button className="transition duration-500 button hover:button_hover focus:outline-none focus:shadow-outline text-gray-700 font-bold py-2 px-4 mx-2" type="submit">
-                    Profile
-              </button>
-              <button className="transition duration-500 button hover:button_hover focus:outline-none focus:shadow-outline text-gray-700 font-bold py-2 px-4 mx-2" type="submit">
-                    About
-              </button>
-              <button className="transition duration-500 button hover:button_hover focus:outline-none focus:shadow-outline text-red-600 font-bold py-2 px-4 mx-2" type="submit">
-                    Logout
-              </button>
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <div className="card_element">
+                    <img className="mx-auto my-auto w-12" src={logo} alt="logo" />
+                </div>
+                {/* <a href="/" className="font-bold text-white text-xl tracking-tight hover:text-yellow-400">Grocery</a> */}
             </div>
-          </div>
+            <div className="block lg:hidden md:hidden">
+                <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                </button>
+            </div>
+            <div className="w-full block lg:w-auto md:w-auto sm:auto ">
+                <div className="text-lg lg:flex-grow md:flex-grow sm:flew-grow">
+                    <button className="transition duration-500 button hover:button_hover focus:outline-none focus:shadow-outline text-gray-700 font-bold py-2 px-4 mx-2" type="submit">
+                        Profile
+              </button>
+                    <button className="transition duration-500 button hover:button_hover focus:outline-none focus:shadow-outline text-gray-700 font-bold py-2 px-4 mx-2" type="submit">
+                        About
+              </button>
+                    <button className="transition duration-500 button hover:button_hover focus:outline-none focus:shadow-outline text-red-600 font-bold py-2 px-4 mx-2" type="submit">
+                        Logout
+              </button>
+                </div>
+            </div>
         </nav>
     )
 }
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -3,22 +3,118 @@ import React, { Component } from 'react'
 import Header from '../Components/header'
 import test from '../assets/images/cloudy-weather.gif'
 
-import Skycons from 'react-skycons'
-
 
 export class Dashboard extends Component {
-    // componentDidMount() {
-    //     var skycons = new Skycons({"color": "pink"});
-
-    //     skycons.add("iconCurrent", Skycons.PARTLY_CLOUDY_DAY);
-    // }
 
     render() {
         return (
-            <div className="container">
+            <div>
                 <Header />
-
-            
+                <section class="text-gray-700s mt-8">
+                    <div class="container card_element px-2 lg:max-h-screen md:max-h-screen sm:mb-0 md:mb-8 h-1/2 py-8 mx-auto bg-gray-100 flex overflow-hidden">
+                        <div class="w-full mx-auto flex flex-wrap">
+                            <div class="lg:w-1/2 w-full lg:pr-10 md:pr-4 p-4 lg:mb-0">
+                                <div class="flex w-full">
+                                    <div class="icon">
+                                        <div class="rays">
+                                            <div class="ray"></div>
+                                            <div class="ray"></div>
+                                            <div class="ray"></div>
+                                            <div class="ray"></div>
+                                        </div>
+                                        <div class="sun"></div>
+                                    </div>
+                                    <div className="text-4xl text-gray-700">90 &#8457;</div>
+                                </div>
+                                <div className="flex float-right sm:m-0 md:m-0 text-xl text-gray-700">Saturday June, 6 2020</div>
+                                <div className="container">
+                                    <div class="flex flex-wrap mt-12 text-center">
+                                        <div class="p-4 lg:w-1/3 md:w-1/3 sm:w-full w-full">
+                                            <div class="card_element4 px-4 py-6 rounded-lg">
+                                                <h2 class="font-medium text-3xl text-green-600">$500.00</h2>
+                                                <p class="leading-relaxed text-gray-700">Income</p>
+                                            </div>
+                                        </div>
+                                        <div class="p-4 lg:w-1/3 md:w-1/3 sm:w-full w-full">
+                                            <div class="card_element4 px-4 py-6 rounded-lg">
+                                                <h2 class="font-medium text-3xl text-red-600">$240.00</h2>
+                                                <p class="leading-relaxed">Expense</p>
+                                            </div>
+                                        </div>
+                                        <div class="p-4 lg:w-1/3 md:w-1/3 sm:w-full w-full">
+                                            <div class="card_element4 px-4 py-6 rounded-lg">
+                                                <h2 class="font-medium text-3xl text-gray-700">$260.00</h2>
+                                                <p class="leading-relaxed">Your balance</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lg:w-1/2 w-full pr-8 py-6">
+                                <div class="flex mb-4">
+                                    <a href="#" class="flex-grow text-gray-700 card_element2 p-4 text-lg"><i class="text-lg fa fa-list mr-2" aria-hidden="true"></i>History</a>
+                                    <a href="#" class="flex-grow text-gray-700 card_element text-lg p-4"><i class="text-lg fa fa-cart-plus mr-2" aria-hidden="true"></i>Add Grocery</a>
+                                </div>
+                                <div className="history-list overflow-y-auto max-h-screen">
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                    <div class="flex flex-col card_element2 p-4 items-center border-r-8 border-green-600 mb-1">
+                                        <h2 class="font-bold">Tomatoes</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         )
     }
