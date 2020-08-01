76e18a647c994ee9a9ed6eeaffaa5c7b80002d3a
Edem Dumenu <ehdemdume@gmail.com>
2020-08-01 17:50:17 -0400

    Purecss
    Adding purgecss to the build to minify project

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,28 @@
 const tailwindcss = require('tailwindcss');
+const cssnano = require('cssnano');
+
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project 
+    content: [
+        './src/**/*.jsx',
+        './src/**/*.js',
+        './public/index.html',
+    ],
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+})
+
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        cssnano({
+            preset: 'default'
+        }),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
     ],
 };
