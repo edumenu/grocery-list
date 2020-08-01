42456dc51736f03be44f66d3cc81208177a86a67
Edem Dumenu <ehdemdume@gmail.com>
2020-08-01 19:53:56 -0400

    Few updates
    Few updates to package file

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/api/v1/user', user)
 const PORT = process.env.PORT || 5000
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build'))
 }
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
